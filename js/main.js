ebbb730495a6b4525f3242f517373f22b3a84d57
Hugo Builder <niclas.mietz+hugobuilder@bee42.com>
2016-12-07 17:08:27 +0000

    CircleCi build #30 with git commit 85f3b9376dea52229e28f6b08b76350024b4d458

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
 
+	// open form
+	$('.open-form').each(function(){
+	 	$(this).click(function(){
+	 		$('.form-container').fadeIn();
+	 		$('.overlay').fadeIn();
+	 		return false;	
+	 	});
+	});
+
+	// select event
+	$('#book-event option').each(function(){
+		var pathname = window.location.pathname; 
+		var getRef = '/'+$(this).data('ref');
+
+		if(getRef == pathname) {
+			$(this).attr('selected',true);
+		}
+	});
+
+	// Close form
+	$('.close').click(function(){
+		$(this).parent().parent().fadeOut();
+		$('.overlay').fadeOut();
+		$('body').css({'overflow': 'scroll'});
+	      $('.form-container').animate({
+	       	scrollTop : 0
+	       });
+	});
+
+	// validate form
+	$(function(){
+		$('form').validate({
+		});
+
+		$.extend($.validator.messages, {
+			required: "This filed is required"
+		});
+	}); 
+
+	// Scroll To (anchor)
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - $('nav.navigation').outerHeight()
+		}, 1000);
+		return false;
+	});		
 });
 
 // cover navigation on scroll with bg color
@@ -12,55 +58,6 @@ $(window).scroll(function() {
 			$('.navigation').removeClass('covered');
 		}
 });
-
-// accordion
-$('.accordion .acc-title').click(function(){
-	$('.acc-content').slideUp();
-	$(this).next().slideDown();
-});
-
-
-// open form
-$('.open-form').each(function(){
-	var getForm = $(this).data('form');
- 	
- 	$(this).click(function(){
- 		$('.form-container').fadeIn();
- 		$('.overlay').fadeIn();
- 		return false;	
- 	});
-	console.log(getForm);
-});
-
-// Close form
-$('.close').click(function(){
-	$(this).parent().parent().fadeOut();
-	$('.overlay').fadeOut();
-	$('body').css({'overflow': 'scroll'});
-      $('.form-container').animate({
-       	scrollTop : 0
-       });
-});
-
-// form validation
-$(function(){
-	$('form').validate({
-	});
-
-	$.extend($.validator.messages, {
-		required: "This filed is required"
-	});
-}); 
-
-
-// Scroll To (anchor)
-$('a').click(function(){
-	$('html, body').animate({
-		scrollTop: $( $(this).attr('href') ).offset().top - $('nav.navigation').outerHeight()
-	}, 1000);
-	return false;
-});	
-
 
 // foundation inits
 $(document).foundation();
